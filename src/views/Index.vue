787311335b98b0da1d69af430f498d43b5fdddb4
Alice <alicexenawong@gmail.com>
2019-12-17 19:01:44 -0800

    Add bootstrap styling.

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,13 +2,21 @@
   <div class="posts-index">
     <h1>{{ message }}</h1>
     <div v-for="post in posts">
-      <p>User id: {{ post.user_id }}</p>
+      <div class="card" style="width: 18rem;">
+        <img v-bind:src="post.image" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{post.title}}</h5>
+          <p class="card-text">{{post.body}}</p>
+          <a v-bind:href="`/posts/${post.id}`" class="btn btn-primary">See More Info</a>
+        </div>
+      </div>
+<!--       <p>User id: {{ post.user_id }}</p>
       <h2>{{ post.title }}</h2>
       <p>{{ post.body }}</p>
-      <img :src="post.image">
-      <br>
+      <img :src="post.image"> -->
+<!--       <br>
       <router-link v-bind:to="`/posts/${post.id}`">Show info</router-link>
-      <hr>
+      <hr> -->
     </div>
   </div>
 </template>
